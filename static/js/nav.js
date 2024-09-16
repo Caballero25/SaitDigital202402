b2368d1ca31bb9e9f99662a12fecdcb0cd7d6daa
David Caballero <davidcame124@gmail.com>
2024-09-16 11:46:15 -0500

    Migrating to webapp on Django

--- static/js/nav.js
+++ static/js/nav.js
@@ -11,11 +11,11 @@ function scrollToSection(event, sectionId) {
     }
 }
 
-var whatsAppUrl = "https://wa.me/593962636961?text=Estoy%20interesado%20en%20recibir%20una%20cotización%20personalizada%20para%20encargar%20mi%20proyecto%20web.";
+var whatsAppUrl = "https://wa.me/593962636961?text=Me%20interesa%20recibir%20una%20cotización%20personalizada%20para%20mi%20proyecto%20web.";
 document.getElementsByClassName("whatsaap")[0].href=whatsAppUrl
 
 
-var gmailUrl = "mailto:saittdocuments@gmail.com?subject=Cotización%20personalizada-%20SaitDigital&body=Estoy%20interesado%20en%20recibir%20una%20cotización%20personalizada%20para%20encargar%20mi%20proyecto%20web."
+var gmailUrl = "mailto:saittdocuments@gmail.com?subject=Cotización%20personalizada-%20SaitDigital&body=Me%20interesa%20recibir%20una%20cotización%20personalizada%20para%20encargar%20mi%20proyecto%20web."
 document.getElementsByClassName("gmail")[0].href=gmailUrl
 
 document.getElementsByClassName("facebook")[0].href = "https://www.facebook.com/saitdigital"
